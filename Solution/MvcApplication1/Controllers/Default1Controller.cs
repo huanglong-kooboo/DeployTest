faaa03ca9aafbeb077222c844cf1e4fa44b64ea5
huanglong-kooboo <huanglong@kooboo.com>
2014-06-02 17:06:02 +0200

    阿道夫adfasd

---
+++ Solution/MvcApplication1/Controllers/Default1Controller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class Default1Controller : Controller
+    {
+        public ActionResult Index()
+        {
+            var a = '123';
+            return View();
+        }
+
+    }
+}
